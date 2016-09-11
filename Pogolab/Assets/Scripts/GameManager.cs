dc98d8494671df9eef833a10afc2d03484de219c
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-09-11 18:07:20 +0100

    updated

--- Pogolab/Assets/Scripts/GameManager.cs
+++ Pogolab/Assets/Scripts/GameManager.cs
@@ -22,7 +22,7 @@ public class GameManager : MonoBehaviour {
 
 
 	public Animator[] animator;
-	public Animation[] animation;
+	//public Animation[] animation;
 	MeshRenderer renderer = null;
 
 	public static string getImageButton = "idle";
@@ -33,16 +33,17 @@ public class GameManager : MonoBehaviour {
 	private RaycastHit hit;
 	private GameObject hitObject = null;
 
+	public GameObject scannerMirror= null;
+	private Sprite OtherSprite;
 
 	void Start () {
-
-
 
 
 		SetupCameraOnDevice ();
 		renderer = mirror.GetComponent<MeshRenderer> ();
 
-
+		Vector3 startPos = new Vector3 (0f,0f,5f);
+		Camera.main.gameObject.transform.position = startPos;
 		//StopAnimation ();
 
 	}
@@ -51,7 +52,7 @@ public class GameManager : MonoBehaviour {
 
 
 		Animating ();
-
+		CameraSnapShot ();
 
 
 	}
@@ -62,20 +63,26 @@ public class GameManager : MonoBehaviour {
 	void SetupCameraOnDevice(){
 
 		devices = WebCamTexture.devices;
+		//devices.
 		deviceName = devices[CAMERADEVICENUMBER].name;
 		//print (deviceName);
 
 		//camBackTex = new WebCamTexture();
 		//camBackTex = new WebCamTexture("FaceTime HD Camera", 512, 512);
+
+		//camBackTex = new WebCamTexture(deviceName, 512, 512);
 		camBackTex = new WebCamTexture(deviceName, 640, 480, 30);
 		//camBackTex  =  new WebCamTexture(deviceName, Screen.width, Screen.height,30);
 
 	}
+
+
 	void CameraSnapShot(){
 		if (getImageButton == "idle") {
 
-			GameObject.Find ("button").GetComponent<MeshRenderer> ().material.color = Color.green;
+			scannerMirror.SetActive(false);
 
+			GameObject.Find ("button").GetComponent<MeshRenderer> ().material.color = Color.green;
 
 			camBackTex.Play ();
 			renderer.material.mainTexture = camBackTex;
@@ -102,15 +109,52 @@ public class GameManager : MonoBehaviour {
 
 			camBackTex = null;
 
-			GameObject.Find ("desk").GetComponent<MeshRenderer> ().material.mainTexture = snaps [0];
+			//GameObject.Find ("desk").GetComponent<MeshRenderer> ().material.mainTexture = snaps [0];
 			renderer.material.mainTexture = snaps[0];
 
+
+
+//			float speed = 2.5f;
+//			float smooth = 1.0f - Mathf.Pow(0.5f, Time.deltaTime * speed);
+//
+//			Vector3 targetPos = new Vector3 (-25f,0,5f);
+//			//Camera.main.gameObject.transform.position = Vector3.Lerp(Camera.main.gameObject.transform.position, targetPos, Time.time);
+//			Camera.main.gameObject.transform.position = Vector3.Lerp(Camera.main.gameObject.transform.position, targetPos, smooth);
+
+			//OtherSprite = new Rect(0, 0, snaps[0].width, snaps[0].height);
+			OtherSprite = Sprite.Create(snaps[0], new Rect(0, 0, 900, 360), new Vector2(-1f, -1f));
+			scannerMirror.GetComponent<Image> ().sprite = OtherSprite;
+
+			scannerMirror.SetActive(true);
 		}
 
 	}
 		
 
-
+	Texture2D ConvertSpriteToTexture(Sprite sprite)
+	{
+		try
+		{
+			if (sprite.rect.width != sprite.texture.width)
+			{
+				Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+				Color[] colors = newText.GetPixels();
+				Color[] newColors = sprite.texture.GetPixels((int)System.Math.Ceiling(sprite.textureRect.x),
+					(int)System.Math.Ceiling(sprite.textureRect.y),
+					(int)System.Math.Ceiling(sprite.textureRect.width),
+					(int)System.Math.Ceiling(sprite.textureRect.height));
+				Debug.Log(colors.Length+"_"+ newColors.Length);
+				newText.SetPixels(newColors);
+				newText.Apply();
+				return newText;
+			}
+			else
+				return sprite.texture;
+		}catch
+		{
+			return sprite.texture;
+		}
+	}
 
 
 	void StopAnimation(){
@@ -167,9 +211,9 @@ public class GameManager : MonoBehaviour {
 
 		if (Physics.Raycast(inputRay, out hit)) {
 
-			hitObject = hit.collider.gameObject;
-			hitObject.GetComponent<Renderer> ().material.color = ExtensionMethods.RandomColor ();
-			Debug.Log("object: "+ hit.collider.gameObject.name);
+//			hitObject = hit.collider.gameObject;
+//			hitObject.GetComponent<Renderer> ().material.color = ExtensionMethods.RandomColor ();
+//			Debug.Log("object: "+ hit.collider.gameObject.name);
 		}
 
 
--- Pogolab/Assets/Scripts/ToothPasteButtonAnim.cs
+++ Pogolab/Assets/Scripts/ToothPasteButtonAnim.cs
@@ -5,24 +5,57 @@ public class ToothPasteButtonAnim : MonoBehaviour {
 
 	Animator anim;
 
+	private GameObject hitObject = null;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
 	void Start () {
-		anim = GetComponent<Animator>();
+		//anim = GetComponent<Animator>();
 	}
 
 	void Update () {
 
-		if ( Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast(ray, out hit)){
-				
-				if(hit.transform.gameObject.name == "controlBoxBar" )
-				{
-					anim.Play(0);
-				}
-				
-			}
-		}
+
+//
+//		if ( Input.GetMouseButtonDown(0))
+//		{
+//			RaycastHit hit;
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//			if (Physics.Raycast(ray, out hit)){
+//
+//				hitObject = hit.collider.gameObject;
+//
+//				//this.transform.localPosition = new Vector3 (0.0f, hit.collider.gameObject.transform.localPosition.y, 0.01f);
+////				if(hit.transform.gameObject.name == "controlBoxBar" )
+////				{
+////					anim.Play(0);
+////				}
+//				
+//			}
+//		}
+
+
 	}
+
+
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+
+		offset = this.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(screenPoint.x, Input.mousePosition.y, screenPoint.z));
+
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(screenPoint.x, Input.mousePosition.y, screenPoint.z);
+
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+
+	}
+
+
+
+
 }
